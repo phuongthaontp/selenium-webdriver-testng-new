70fc66124a9eeabc3d8264e9223f3810ca3dc3e1
Dinh Thi Huyen Trang <miss.trang57@gmail.com>
2022-09-15 00:35:38 +0700

    add

--- selenium/webdriver/Topic_04.java
+++ selenium/webdriver/Topic_04.java
@@ -20,23 +20,62 @@ public class Topic_04 {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://pastecode.io/s/zztef7o7");
+		driver.get("https://vnext.vn");
 	}
 
 	@Test
 	public void TC_01_VerifyUrl() {
-		String techPageUrl = driver.getCurrentUrl();
-		Assert.assertEquals(techPageUrl, "https://pastecode.io/s/zztef7o7");
-		/* WebElement myAccountfooter = driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']"));
-		myAccountfooter.click();
-		String techPageUrllogin = driver.getCurrentUrl();
-		Assert.assertEquals(techPageUrllogin, "http://www.live.techpanda.org/index.php/customer/account/login");
-		WebElement createAnAcountbtn = driver.findElement(By.xpath("//span[contains(text(),'Create An Account')"));
-		createAnAcountbtn.click();
-		String techPageUrlregister = driver.getCurrentUrl();
-		Assert.assertEquals(techPageUrlregister, "http://www.live.techpanda.org/index.php/customer/account/create");
-		driver.quit();
-*/
-
+		String vnextUrl = driver.getCurrentUrl();
+		Assert.assertEquals(vnextUrl, "https://vnext.vn/");
+		WebElement serviceLabofooter = driver.findElement(By.xpath("//a[contains(text(),'Phát triển theo hình thức Labo')]"));
+		serviceLabofooter.click();
+		String serviceLaboUrl = driver.getCurrentUrl();
+		Assert.assertEquals(serviceLaboUrl, "https://vnext.vn/vi-vn/service-labo.html");
+		WebElement contactbtn = driver.findElement(By.xpath("//a[@class='skewed-button d-row download-doc-button']//p[contains(text(),'Liên hệ')]"));
+		contactbtn.click();
+		String contactUrl = driver.getCurrentUrl();
+		Assert.assertEquals(contactUrl, "https://vnext.vn/vi-vn/intruction-contact.html");
 }
+	@Test
+	public void TC_02_VerifyTitle() {
+		driver.get("https://vnext.vn");;
+		WebElement serviceLabofooter = driver.findElement(By.xpath("//a[contains(text(),'Phát triển theo hình thức Labo')]"));
+		serviceLabofooter.click();
+		String serviceLaboTitle = driver.getTitle();
+		Assert.assertEquals(serviceLaboTitle, "VNEXT HOLDINGS | Dịch vụ | Phát triển theo hình thức Labo");
+		WebElement contactbtn = driver.findElement(By.xpath("//a[@class='skewed-button d-row download-doc-button']//p[contains(text(),'Liên hệ')]"));
+		contactbtn.click();
+		String contactTitle = driver.getTitle();
+		Assert.assertEquals(contactTitle, "VNEXT HOLDINGS | Liên hệ");
+	}
+	
+	@Test
+	public void TC_03_NavigateFuntion() {
+		driver.get("https://vnext.vn");;
+		WebElement serviceLabofooter = driver.findElement(By.xpath("//a[contains(text(),'Phát triển theo hình thức Labo')]"));
+		serviceLabofooter.click();
+		WebElement contactbtn = driver.findElement(By.xpath("//a[@class='skewed-button d-row download-doc-button']//p[contains(text(),'Liên hệ')]"));
+		contactbtn.click();
+		String contactUrl = driver.getCurrentUrl();
+		Assert.assertEquals(contactUrl, "https://vnext.vn/vi-vn/intruction-contact.html");
+		driver.navigate().back();
+		String serviceLaboUrl = driver.getCurrentUrl();
+		Assert.assertEquals(serviceLaboUrl, "https://vnext.vn/vi-vn/service-labo.html");
+		driver.navigate().forward();
+		String contactTitle = driver.getTitle();
+		Assert.assertEquals(contactTitle, "VNEXT HOLDINGS | Liên hệ");
+	}
+	@Test
+	public void TC_04_GetPageSource() {
+		driver.get("https://vnext.vn");;
+		WebElement serviceLabofooter = driver.findElement(By.xpath("//a[contains(text(),'Phát triển theo hình thức Labo')]"));
+		serviceLabofooter.click();
+		String laboPageSource = driver.getPageSource();
+		Assert.assertTrue(laboPageSource.contains("cung cấp các loại hình phát triển đa dạng"));
+		WebElement contactbtn = driver.findElement(By.xpath("//a[@class='skewed-button d-row download-doc-button']//p[contains(text(),'Liên hệ')]"));
+		contactbtn.click();
+		String contactPageSource = driver.getPageSource();
+		Assert.assertTrue(contactPageSource.contains("Liên hệ về dịch vụ"));
+		driver.quit();
+	}
 }
---
+++ selenium/webdriver/Topic_04_Browser_Part1.java
@@ -0,0 +1,44 @@
+package webdriver;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_04_Browser_Part1 {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+
+	@Test
+	public void TC_01_Browser() {
+	}
+
+	@Test
+	public void TC_02_Element() {
+		
+	}
+	@Test
+	public void TC_03_Method() {
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
